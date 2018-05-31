9969a8157de0148150ac58e076c1a112868f55ec
SebastienCadorette <cado.94@me.com>
2018-05-31 09:25:02 -0400

    Adding some more tests and a command in the package.json to fix some of the CLI errors.

--- client/src/app/crosswords/definition/definition.component.shallow.spec.ts
+++ client/src/app/crosswords/definition/definition.component.shallow.spec.ts
@@ -4,12 +4,12 @@ import { ServiceInteractionComponent } from "../service-interaction-component/se
 import { of } from "rxjs/observable/of";
 import { Mot } from "../objetsTest/mot";
 import { grilleLettres } from "../objetsTest/objetsTest";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 describe("Definition (shallow tests)", () => {
     let fixture: ComponentFixture<DefinitionComponent>;
     let mockListeMotsService: jasmine.SpyObj<ServiceInteractionComponent>;
-    let MOTS: Mot[];
-    const MOT: Mot = {
+    const MOT_NON_TROUVE: Mot = {
         definitions: [{ definition: "beneath the surface of the water" }],
         estVertical: false,
         longueur: 10,
@@ -21,29 +21,42 @@ describe("Definition (shallow tests)", () => {
         cheat: false,
         positionsLettres: []
     };
+    const MOT_TROUVE: Mot = {
+        definitions: [{definition: "destroyed physically or morally"}],
+        estVertical: true,
+        longueur: 9,
+        mot: "destroyed",
+        premierX: 2,
+        premierY: 0,
+        activer: false,
+        motTrouve: true,
+        cheat: false,
+        positionsLettres: []
+    };
+    const MOTS: Mot[] = [
+        {
+            definitions: [{ definition: "beneath the surface of the water" }],
+            estVertical: false,
+            longueur: 10,
+            mot: "underwater",
+            premierX: 0,
+            premierY: 0,
+            activer: false,
+            motTrouve: false,
+            cheat: false,
+            positionsLettres: []
+        }
+    ];
 
     beforeEach(() => {
-        MOTS = [
-            {
-                definitions: [{ definition: "beneath the surface of the water" }],
-                estVertical: false,
-                longueur: 10,
-                mot: "underwater",
-                premierX: 0,
-                premierY: 0,
-                activer: false,
-                motTrouve: false,
-                cheat: false,
-                positionsLettres: []
-            }
-        ];
         mockListeMotsService = jasmine.createSpyObj([
             "serviceReceptionMotSelectionne", "serviceReceptionMots",
-            "serviceReceptionMatriceLettres", "serviceReceptionMotTrouve", "serviceReceptionMotPerdu",
+            "serviceReceptionMatriceLettres",
             "serviceEnvoieMotSelectionne", "serviceEnvoieMatriceLettres"]);
         TestBed.configureTestingModule({
             declarations: [DefinitionComponent],
-            providers: [{ provide: ServiceInteractionComponent, useValue: mockListeMotsService }]
+            providers: [{ provide: ServiceInteractionComponent, useValue: mockListeMotsService }],
+            schemas: [NO_ERRORS_SCHEMA]
         });
 
         fixture = TestBed.createComponent(DefinitionComponent);
@@ -53,28 +66,62 @@ describe("Definition (shallow tests)", () => {
         expect(true).toBe(true);
     });
 
-    it("should set mots corretly from the service", () => {
+    it("should set mots correctly from the service", () => {
         mockListeMotsService.serviceReceptionMots.and.returnValue(of(MOTS));
-        fixture.detectChanges();
+
+        fixture.componentInstance["souscrireReceptionMots"]();
 
         expect(fixture.componentInstance.mots.length).toBe(1);
-        expect(fixture.componentInstance.mots[0]).toBe(MOTS[0]);
     });
 
-    describe("should set motSelectionne correctly from the service", () => {
-        it("by having the correct word", () => {
-            mockListeMotsService.serviceReceptionMotSelectionne.and.returnValue(of(MOT));
-            fixture.detectChanges();
+    it("should set motSelectionne correctly from the service", () => {
+        const spyFunction1: jasmine.Spy = spyOn<any>(fixture.componentInstance, "miseAJourMotSelectionne");
+        fixture.componentInstance.mots = MOTS;
+        mockListeMotsService.serviceReceptionMotSelectionne.and.returnValue(of(MOT_NON_TROUVE));
 
-            expect(fixture.componentInstance.motSelectionne).toBe(MOT);
-        });
+        fixture.componentInstance["souscrireSelectionMots"]();
+
+        expect(spyFunction1).toHaveBeenCalled();
+        expect(fixture.componentInstance.motSelectionne).toBe(MOT_NON_TROUVE);
     });
 
     it("should set matriceDesMotsSurGrille correctly", () => {
         mockListeMotsService.serviceReceptionMatriceLettres.and.returnValue(of(grilleLettres));
-        fixture.detectChanges();
 
-        expect(fixture.componentInstance.matriceDesMotsSurGrille.length).toBe(grilleLettres.length);
-        expect(fixture.componentInstance.matriceDesMotsSurGrille[0]).toBe(grilleLettres[0]);
+        fixture.componentInstance["souscrireReceptionMatrice"]();
+
+        expect(fixture.componentInstance.matriceDesMotsSurGrille.length).toBe(10);
+    });
+
+    it("should change the selected word if word not already found", () => {
+        const spyFunction1: jasmine.Spy = spyOn<any>(fixture.componentInstance, "miseAJourMotSelectionne");
+        const spyFunction2: jasmine.Spy = spyOn<any>(fixture.componentInstance, "envoieMotSelectionne");
+        fixture.componentInstance.mots = MOTS;
+        fixture.componentInstance.matriceDesMotsSurGrille = grilleLettres;
+
+        fixture.componentInstance.changementMotSelectionne(MOT_NON_TROUVE);
+
+        expect(spyFunction1).toHaveBeenCalled();
+        expect(spyFunction2).toHaveBeenCalled();
+    });
+
+    it("should not change the selected word if word already found", () => {
+        const spyFunction1: jasmine.Spy = spyOn<any>(fixture.componentInstance, "miseAJourMotSelectionne");
+        const spyFunction2: jasmine.Spy = spyOn<any>(fixture.componentInstance, "envoieMotSelectionne");
+        fixture.componentInstance.mots = MOTS;
+        fixture.componentInstance.matriceDesMotsSurGrille = grilleLettres;
+
+        fixture.componentInstance.changementMotSelectionne(MOT_TROUVE);
+
+        expect(spyFunction1).not.toHaveBeenCalled();
+        expect(spyFunction2).not.toHaveBeenCalled();
+    });
+
+    it("should call serviceEnvoieMotSelectionne from the service when calling envoieMotSelectionne", () => {
+        fixture.componentInstance.motSelectionne = MOT_NON_TROUVE;
+
+        fixture.componentInstance["envoieMotSelectionne"]();
+
+        expect(mockListeMotsService.serviceEnvoieMotSelectionne).toHaveBeenCalled();
     });
 });
--- client/src/app/crosswords/definition/definition.component.ts
+++ client/src/app/crosswords/definition/definition.component.ts
@@ -4,132 +4,133 @@ import { Subscription } from "rxjs/Subscription";
 import { ServiceInteractionComponent } from "../service-interaction-component/service-interaction-component";
 import { Mot } from "../objetsTest/mot";
 import { LettreGrille } from "../objetsTest/lettreGrille";
-import { OpaciteCase} from "../grille/librairieGrille/opaciteCase";
+import { OpaciteCase } from "../grille/librairieGrille/opaciteCase";
 
 @Component({
-  selector: "app-definition",
-  templateUrl: "./definition.component.html",
-  styleUrls: ["./definition.component.css"]
+    selector: "app-definition",
+    templateUrl: "./definition.component.html",
+    styleUrls: ["./definition.component.css"]
 })
 
 export class DefinitionComponent implements OnInit, OnDestroy {
-  private _mots: Mot[];
-  private _matriceDesMotsSurGrille: Array<Array<LettreGrille>>;
+    private _mots: Mot[];
+    private _matriceDesMotsSurGrille: Array<Array<LettreGrille>>;
+    private _motSelectionne: Mot;
 
-  private subscriptionMots: Subscription;
-  private subscriptionMatrice: Subscription;
-  private subscriptionMotSelec: Subscription;
-  private subscriptionMotTrouve: Subscription;
-  private subscriptionMotPerdu: Subscription;
+    private subscriptionMots: Subscription;
+    private subscriptionMatrice: Subscription;
+    private subscriptionMotSelec: Subscription;
+    private subscriptionMotTrouve: Subscription;
+    private subscriptionMotPerdu: Subscription;
 
-  private _motSelectionne: Mot;
-
-  public constructor(private listeMotsService: ServiceInteractionComponent) {
-    this._mots = this.listeMotsService.mots;
-    this._matriceDesMotsSurGrille = this.listeMotsService.matrice;
-  }
-
-  public ngOnInit(): void {
-    this.initialiserSouscriptions();
-  }
-
-  public ngOnDestroy(): void {
-    this.subscriptionMots.unsubscribe();
-    this.subscriptionMatrice.unsubscribe();
-    this.subscriptionMotSelec.unsubscribe();
-  }
-
-  public get mots(): Mot[] {
-      return this._mots;
-  }
-
-  public set mots(desMots: Mot[]) {
-      this._mots = desMots;
-  }
-
-  public get matriceDesMotsSurGrille(): Array<Array<LettreGrille>> {
-      return this._matriceDesMotsSurGrille;
-  }
-
-  public set matriceDesMotsSurGrille(matrice: Array<Array<LettreGrille>>) {
-      this._matriceDesMotsSurGrille = matrice;
-  }
-
-  public get motSelectionne(): Mot {
-    return this._motSelectionne;
-}
-
-public set motSelectionne(mot: Mot) {
-    this._motSelectionne = mot;
-}
-
-  // Souscriptions
-
-  private initialiserSouscriptions(): void {
-    this.souscrireReceptionMots();
-    this.souscrireSelectionMots();
-    this.souscrireReceptionMatrice();
-    this.souscrireMotsTrouves();
-    this.souscrireMotsPerdus();
-  }
-
-  private souscrireReceptionMots(): void {
-    this.subscriptionMots = this.listeMotsService.serviceReceptionMots()
-      .subscribe((mots) => this._mots = mots);
-  }
-
-  private souscrireSelectionMots(): void {
-    this.subscriptionMotSelec = this.listeMotsService.serviceReceptionMotSelectionne().subscribe((motSelect) => {
-      this._motSelectionne = motSelect;
-      this.miseAJourMotSelectionne(this._motSelectionne);
-    });
-  }
-
-  private souscrireReceptionMatrice(): void {
-    this.subscriptionMatrice = this.listeMotsService.serviceReceptionMatriceLettres()
-      .subscribe((matrice) => this._matriceDesMotsSurGrille = matrice);
-  }
-
-  private souscrireMotsTrouves(): void {
-    this.subscriptionMotTrouve = this.listeMotsService.serviceReceptionMotTrouve()
-      .subscribe((mot: Mot) => document.getElementById(mot.mot).classList.add("motTrouve"));
-  }
-
-  private souscrireMotsPerdus(): void {
-    this.subscriptionMotPerdu = this.listeMotsService.serviceReceptionMotPerdu()
-      .subscribe((mot: Mot) => document.getElementById(mot.mot).classList.add("motPerdu"));
-  }
-
-  // Changement d'un mot
-
-  public changementMotSelectionne(mot: Mot): void {
-    if (!mot.motTrouve) {
-      this.miseAJourMotSelectionne(mot);
-      this.envoieMotSelectionne();
+    public constructor(private listeMotsService: ServiceInteractionComponent) {
+        this._mots = this.listeMotsService.mots;
+        this._matriceDesMotsSurGrille = this.listeMotsService.matrice;
     }
-  }
 
-  private envoieMotSelectionne(): void {
-    this.listeMotsService.serviceEnvoieMotSelectionne(this._motSelectionne);
-  }
+    public ngOnInit(): void {
+        this.initialiserSouscriptions();
+    }
 
-  private miseAJourMotSelectionne(mot: Mot): void {
-    this.changementMot(mot);
-    this.decouvrirCases(mot);
-  }
+    public ngOnDestroy(): void {
+        this.subscriptionMots.unsubscribe();
+        this.subscriptionMatrice.unsubscribe();
+        this.subscriptionMotSelec.unsubscribe();
+        this.subscriptionMotTrouve.unsubscribe();
+        this.subscriptionMotPerdu.unsubscribe();
+    }
 
-  private changementMot(mot: Mot): void {
-    this._mots.forEach((element: Mot) => element.activer = false);
-    this._motSelectionne = mot;
-    mot.activer = !mot.activer;
-  }
+    public get mots(): Mot[] {
+        return this._mots;
+    }
 
-  private decouvrirCases(mot: Mot): void {
-    OpaciteCase.decouvrirCases(mot, this._matriceDesMotsSurGrille);
-    this.envoieMatrice();
-  }
+    public set mots(desMots: Mot[]) {
+        this._mots = desMots;
+    }
 
-  private envoieMatrice(): void {
-    this.listeMotsService.serviceEnvoieMatriceLettres(this._matriceDesMotsSurGrille);
-  }
+    public get matriceDesMotsSurGrille(): Array<Array<LettreGrille>> {
+        return this._matriceDesMotsSurGrille;
+    }
+
+    public set matriceDesMotsSurGrille(matrice: Array<Array<LettreGrille>>) {
+        this._matriceDesMotsSurGrille = matrice;
+    }
+
+    public get motSelectionne(): Mot {
+        return this._motSelectionne;
+    }
+
+    public set motSelectionne(mot: Mot) {
+        this._motSelectionne = mot;
+    }
+
+    // Souscriptions
+
+    private initialiserSouscriptions(): void {
+        this.souscrireReceptionMots();
+        this.souscrireSelectionMots();
+        this.souscrireReceptionMatrice();
+        this.souscrireMotsTrouves();
+        this.souscrireMotsPerdus();
+    }
+
+    private souscrireReceptionMots(): void {
+        this.subscriptionMots = this.listeMotsService.serviceReceptionMots()
+            .subscribe((mots) => this._mots = mots);
+    }
+
+    private souscrireSelectionMots(): void {
+        this.subscriptionMotSelec = this.listeMotsService.serviceReceptionMotSelectionne().subscribe((motSelect) => {
+            this._motSelectionne = motSelect;
+            this.miseAJourMotSelectionne(this._motSelectionne);
+        });
+    }
+
+    private souscrireReceptionMatrice(): void {
+        this.subscriptionMatrice = this.listeMotsService.serviceReceptionMatriceLettres()
+            .subscribe((matrice) => this._matriceDesMotsSurGrille = matrice);
+    }
+
+    private souscrireMotsTrouves(): void {
+        this.subscriptionMotTrouve = this.listeMotsService.serviceReceptionMotTrouve()
+            .subscribe((mot: Mot) => document.getElementById(mot.mot).classList.add("motTrouve"));
+    }
+
+    private souscrireMotsPerdus(): void {
+        this.subscriptionMotPerdu = this.listeMotsService.serviceReceptionMotPerdu()
+            .subscribe((mot: Mot) => document.getElementById(mot.mot).classList.add("motPerdu"));
+    }
+
+    // Changement d'un mot
+
+    public changementMotSelectionne(mot: Mot): void {
+        if (!mot.motTrouve) {
+            this.miseAJourMotSelectionne(mot);
+            this.envoieMotSelectionne();
+        }
+    }
+
+    private envoieMotSelectionne(): void {
+        this.listeMotsService.serviceEnvoieMotSelectionne(this._motSelectionne);
+    }
+
+    private miseAJourMotSelectionne(mot: Mot): void {
+        this.changementMot(mot);
+        this.decouvrirCases(mot);
+    }
+
+    private changementMot(mot: Mot): void {
+        this._mots.forEach((element: Mot) => element.activer = false);
+        this._motSelectionne = mot;
+        mot.activer = !mot.activer;
+    }
+
+    private decouvrirCases(mot: Mot): void {
+        OpaciteCase.decouvrirCases(mot, this._matriceDesMotsSurGrille);
+        this.envoieMatrice();
+    }
+
+    private envoieMatrice(): void {
+        this.listeMotsService.serviceEnvoieMatriceLettres(this._matriceDesMotsSurGrille);
+    }
 }
