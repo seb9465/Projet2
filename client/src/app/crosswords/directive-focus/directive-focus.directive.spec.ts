04ae259659852558e4413687ff848f4bb4ccf2dd
Sebastien Cadorette <cado.94@me.com>
2018-06-22 15:20:05 -0400

    Suppressed all the tests.

--- client/src/app/crosswords/directive-focus/directive-focus.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DirectiveFocusDirective } from "./directive-focus.directive";
-
-describe("DirectiveFocusDirective", () => {
-  it("should create an instance", () => {
-    const directive: DirectiveFocusDirective = new DirectiveFocusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
