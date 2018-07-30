52141f68e7a88011fc014921c9203c8431d3c037
SebastienCadorette <cado.94@me.com>
2018-07-30 14:38:17 -0400

    Multiple tests added. Everything passing.

--- client/src/app/crosswords/definition/definition.component.spec.ts
+++ client/src/app/crosswords/definition/definition.component.spec.ts
@@ -1,8 +1,149 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { DefinitionComponent } from "./definition.component";
 import { ServiceInteractionComponent } from "../service-interaction-component/service-interaction-component";
+import { Mot } from "../objetsTest/mot";
+import { of } from "../../../../node_modules/rxjs/observable/of";
+import { LettreGrille } from "./../objetsTest/lettreGrille";
 
 describe("DefinitionComponent", () => {
+    const mockMatrice: LettreGrille[][] = [
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "o", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "u", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "t", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "a", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "r", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "d", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "e", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+        [
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false },
+            { caseDecouverte: false, lettre: "m", lettreDecouverte: false }
+        ],
+    ];
+    const unMot: Mot = {
+        mot: "moutarde",
+        definitions: undefined,
+        estVertical: false,
+        longueur: 8,
+        premierX: 0,
+        premierY: 0,
+        activer: false,
+        motTrouve: false,
+        cheat: false,
+        positionsLettres: []
+    };
+    const mockMots: Mot[] = [
+        unMot
+    ];
+    const mockMot: Mot = unMot;
     let fixture: ComponentFixture<DefinitionComponent>;
     let mockServiceInteractionComp: jasmine.SpyObj<ServiceInteractionComponent>;
 
@@ -32,9 +173,7 @@ describe("DefinitionComponent", () => {
         fixture = TestBed.createComponent(DefinitionComponent);
     });
 
-    afterEach(() => {
-
-    });
+    afterEach(() => {});
 
     it("should do nothing", () => {
         expect(true).toBe(true);
@@ -42,5 +181,99 @@ describe("DefinitionComponent", () => {
 
     it("should be define", () => {
         expect(fixture).toBeDefined();
+    });
+
+    describe("Subscriptions", () => {
+        describe("Reception Mots", () => {
+            it("Service's function should be called to return a value.", () => {
+                mockServiceInteractionComp.serviceReceptionMots.and.returnValue(
+                    of(mockMots)
+                );
+
+                fixture.componentInstance["souscrireReceptionMots"]();
+
+                expect(
+                    mockServiceInteractionComp.serviceReceptionMots
+                ).toHaveBeenCalled();
+            });
+
+            it("Current component should receive the good value from the service.", () => {
+                mockServiceInteractionComp.serviceReceptionMots.and.returnValue(
+                    of(mockMots)
+                );
+
+                fixture.componentInstance["souscrireReceptionMots"]();
+
+                expect(fixture.componentInstance.mots).toBe(mockMots);
+            });
+        });
+        describe("Reception Matrice", () => {
+            it("Service's function should be called to return a value.", () => {
+                mockServiceInteractionComp.serviceReceptionMatriceLettres.and.returnValue(of(mockMatrice));
+
+                fixture.componentInstance["souscrireReceptionMatrice"]();
+
+                expect(mockServiceInteractionComp.serviceReceptionMatriceLettres).toHaveBeenCalled();
+            });
+            it("Current component should receive the good value from the service.", () => {
+                mockServiceInteractionComp.serviceReceptionMatriceLettres.and.returnValue(of(mockMatrice));
+
+                fixture.componentInstance["souscrireReceptionMatrice"]();
+
+                expect(fixture.componentInstance.matriceDesMotsSurGrille).toBe(mockMatrice);
+            });
+        });
+        describe("Reception Mot Trouve", () => {
+            it("Service's function should be called to return a value.", () => {
+                mockServiceInteractionComp.serviceReceptionMotTrouve.and.returnValue(of(mockMot));
+
+                fixture.componentInstance["souscrireMotsTrouves"]();
+
+                expect(mockServiceInteractionComp.serviceReceptionMotTrouve).toHaveBeenCalled();
+            });
+        });
+        describe("Reception Mot Perdu", () => {
+            it("Services's function shoudl be called to return a value.", () => {
+                mockServiceInteractionComp.serviceReceptionMotPerdu.and.returnValue(of(mockMot));
+
+                fixture.componentInstance["souscrireMotsPerdus"]();
+
+                expect(mockServiceInteractionComp.serviceReceptionMotPerdu).toHaveBeenCalled();
+            });
+        });
+        describe("Envoie Matrice Lettres", () => {
+            it("Should call the service's function.", () => {
+                fixture.componentInstance["envoieMatrice"]();
+
+                expect(mockServiceInteractionComp.serviceEnvoieMatriceLettres).toHaveBeenCalled();
+            });
+        });
+        describe("Envoie Mot Selectionne", () => {
+            it("Should call the service's function.", () => {
+                fixture.componentInstance["envoieMotSelectionne"]();
+
+                expect(mockServiceInteractionComp.serviceEnvoieMotSelectionne).toHaveBeenCalled();
+            });
+        });
+    });
+    it("decouvrir case", () => {
+        const spy = spyOn<any>(DefinitionComponent.prototype, "envoieMatrice");
+        fixture.componentInstance.matriceDesMotsSurGrille = mockMatrice;
+
+        fixture.componentInstance["decouvrirCases"](unMot);
+
+        expect(spy).toHaveBeenCalled();
+        // expect(fixture.componentInstance["decouvrirCases"]).toHaveBeenCalled();
+        for (let indice: number = 0; indice < unMot.longueur; indice++) {
+            expect(fixture.componentInstance.matriceDesMotsSurGrille[unMot.premierX + indice][unMot.premierY]).toBeTruthy();
+        }
+    });
+    it("Decouvrir cases", () => {
+        const spy = spyOn<any>(DefinitionComponent.prototype, "envoieMatrice");
+        fixture.componentInstance.matriceDesMotsSurGrille = mockMatrice;
+
+        fixture.componentInstance["decouvrirCases"](unMot);
+
+        expect(spy).toHaveBeenCalled();
     });
 });
