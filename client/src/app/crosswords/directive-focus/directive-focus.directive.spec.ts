4e94e9859aee2e9715caaf166eb0a1effe7e902a
SebastienCadorette <cado.94@me.com>
2018-07-31 14:05:33 -0400

    Directive focus tests added.

--- client/src/app/crosswords/directive-focus/directive-focus.directive.spec.ts
+++ client/src/app/crosswords/directive-focus/directive-focus.directive.spec.ts
@@ -1,34 +1,89 @@
-import { TestBed } from "../../../../node_modules/@angular/core/testing";
 import { DirectiveFocusDirective } from "./directive-focus.directive";
-import { Component, DebugElement } from "../../../../node_modules/@angular/core";
-import { ComponentFixture } from "@angular/core/testing";
-import { By } from "../../../../node_modules/@angular/platform-browser";
-// import { KEYCODE_MIN } from './../constantes';
+// import { Component } from "../../../../node_modules/@angular/core";
 
-@Component({
-    template: `<input type="text" appDirectiveFocus>`
-})
-class TestAppDirectiveFocusComponent { }
+// @Component({
+//     template: `<input type="text" appDirectiveFocus>`
+// })
+// class TestAppDirectiveFocusComponent { }
 
 describe("Directive Focus", () => {
+    it("Should create an instance.", () => {
+        const directive: DirectiveFocusDirective = new DirectiveFocusDirective();
 
-    let component: TestAppDirectiveFocusComponent;
-    let fixture: ComponentFixture<TestAppDirectiveFocusComponent>;
-    let inputEl: DebugElement;
+        expect(directive).toBeTruthy();
+    });
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [ TestAppDirectiveFocusComponent, DirectiveFocusDirective]
+    describe("EstUneLettre function.", () => {
+        let directive: DirectiveFocusDirective;
+
+        beforeEach(() => {
+            directive = new DirectiveFocusDirective();
         });
-        fixture = TestBed.createComponent(TestAppDirectiveFocusComponent);
-        component = fixture.componentInstance;
-        inputEl = fixture.debugElement.query(By.css("input"));
-    });
 
-    it("Should do nothing.", () => {
-        expect(true).toBe(true);
-    });
-    it("Should be define.", () => {
-        expect(fixture).toBeDefined();
+        describe("Should be false", () => {
+            it("When giving ArrowDown.", () => {
+                const keyboardEvent: KeyboardEvent = new KeyboardEvent("keydown", { key: "ArrowDown" });
+
+                const result: boolean = directive["estUneLettre"](keyboardEvent);
+
+                expect(result).toBeFalsy();
+            });
+            it("When giving Backspace.", () => {
+                const keyboardEvent: KeyboardEvent = new KeyboardEvent("keydown", { key: "Backspace" });
+
+                const result: boolean = directive["estUneLettre"](keyboardEvent);
+
+                expect(result).toBeFalsy();
+            });
+            it("When giving Enter.", () => {
+                const keyboardEvent: KeyboardEvent = new KeyboardEvent("keydown", { key: "Enter" });
+
+                const result: boolean = directive["estUneLettre"](keyboardEvent);
+
+                expect(result).toBeFalsy();
+            });
+            it("When giving Undo.", () => {
+                const keyboardEvent: KeyboardEvent = new KeyboardEvent("keydown", { key: "Undo" });
+
+                const result: boolean = directive["estUneLettre"](keyboardEvent);
+
+                expect(result).toBeFalsy();
+            });
+        });
+
+        describe("Should be true", () => {
+            it("When giving the letter a", () => {
+                const keyboardEvent: KeyboardEvent = new KeyboardEvent("keydown", { key: "a" });
+
+                const result: boolean = directive["estUneLettre"](keyboardEvent);
+
+                expect(result).toBeTruthy();
+            });
+            it("When giving the letter z", () => {
+                const keyboardEvent: KeyboardEvent = new KeyboardEvent("keydown", { key: "z" });
+
+                const result: boolean = directive["estUneLettre"](keyboardEvent);
+
+                expect(result).toBeTruthy();
+            });
+            it("When giving the letter g", () => {
+                const keyboardEvent: KeyboardEvent = new KeyboardEvent("keydown", { key: "g" });
+
+                const result: boolean = directive["estUneLettre"](keyboardEvent);
+
+                expect(result).toBeTruthy();
+            });
+            it("When giving the letter r", () => {
+                const keyboardEvent: KeyboardEvent = new KeyboardEvent("keydown", { key: "r" });
+
+                const result: boolean = directive["estUneLettre"](keyboardEvent);
+
+                expect(result).toBeTruthy();
+            });
+        });
+
+        afterEach(() => {
+            directive = null;
+        });
     });
 });
