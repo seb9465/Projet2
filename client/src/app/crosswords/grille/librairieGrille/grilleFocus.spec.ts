5e941786a3aa61b831cb0a13bb54af1226ca6b14
SebastienCadorette <cado.94@me.com>
2018-07-31 14:52:53 -0400

    Added test file for the Grille Focus class.

---
+++ client/src/app/crosswords/grille/librairieGrille/grilleFocus.spec.ts
@@ -0,0 +1,10 @@
+// import { GrilleFocus } from "./grilleFocus";
+
+describe("Grille Focus", () => {
+    // const mockDocument: Document = new Document();
+    // const component: GrilleFocus = new GrilleFocus(mockDocument, 0);
+
+    it("should do nothing", () => {
+        expect(true).toBe(true);
+    });
+});
