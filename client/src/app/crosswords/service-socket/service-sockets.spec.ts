d0d35b3d8ace405e99497f971f04b2ae23e2c70b
SebastienCadorette <cado.94@me.com>
2018-08-04 09:27:32 -0400

    Removed unused variable.

--- client/src/app/crosswords/service-socket/service-sockets.spec.ts
+++ client/src/app/crosswords/service-socket/service-sockets.spec.ts
@@ -1,5 +1,5 @@
 
-import { SocketService, SERVER_URL } from "./service-socket";
+import { SocketService } from "./service-socket";
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { DIFFICULTE_DEFAUT } from './../serviceHttp/http-request.service';
