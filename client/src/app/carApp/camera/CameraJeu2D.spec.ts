532485c1bb528dab05e6767ee48fef7410c4e1be
Sebastien Cadorette <cado.94@me.com>
2018-08-20 22:28:15 -0400

    Added a couple of tests.

--- client/src/app/carApp/camera/CameraJeu2D.spec.ts
+++ client/src/app/carApp/camera/CameraJeu2D.spec.ts
@@ -1,4 +1,5 @@
-import { CameraJeu2D, PLAN_ELOIGNE, PLAN_RAPPROCHE, ZOOM_DEFAUT } from './CameraJeu2D';
+import * as assert from "assert";
+import { CameraJeu2D, PLAN_ELOIGNE, PLAN_RAPPROCHE, ZOOM_DEFAUT, ZOOM_MINIMUM, ZOOM_MAXIMUM, PAS_ZOOM } from "./CameraJeu2D";
 
 describe("Camera Jeu 2D Class", () => {
     let cls: CameraJeu2D;
@@ -21,6 +22,35 @@ describe("Camera Jeu 2D Class", () => {
         it("Should initialize the plan values", () => {
             expect(cls["_camera"].near).toEqual(PLAN_RAPPROCHE);
             expect(cls["_camera"].far).toEqual(PLAN_ELOIGNE);
+        });
+    });
+
+    describe("Constantes pour zoom", () => {
+        it("Zoom minimal est positif et <= zoom maximal", () => {
+            assert(ZOOM_MINIMUM <= ZOOM_MAXIMUM && ZOOM_MINIMUM >= 0);
+        });
+
+        it("Zoom maximal est positif et >= zoom minimal", () => {
+            assert(ZOOM_MAXIMUM >= ZOOM_MINIMUM && ZOOM_MAXIMUM >= 0);
+        });
+
+        it("Zoom par defaut est borne par valeur min et max", () => {
+            assert(ZOOM_MINIMUM <= ZOOM_DEFAUT && ZOOM_DEFAUT <= ZOOM_MAXIMUM);
+        });
+
+        it("Pas du zoom permet un nombre de gradations suffisantes", () => {
+            const NOMBRE_GRADATIONS: number = 10;
+            assert((ZOOM_MAXIMUM - ZOOM_MINIMUM) / PAS_ZOOM >= NOMBRE_GRADATIONS);
+        });
+    });
+
+    describe("Constantes pour camera", () => {
+        it("Plan rapproche est valide", () => {
+            assert(PLAN_RAPPROCHE < PLAN_ELOIGNE);
+        });
+
+        it("Plan eloigne est valide", () => {
+            assert(PLAN_ELOIGNE >= 0 && PLAN_ELOIGNE > PLAN_RAPPROCHE);
         });
     });
 
