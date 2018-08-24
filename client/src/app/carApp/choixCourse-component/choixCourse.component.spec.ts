e26ac22da1a0fafba4e3528a5620afe0416bca0a
SebastienCadorette <cado.94@me.com>
2018-08-23 20:11:44 -0400

    Component configuration.

--- client/src/app/carApp/choixCourse-component/choixCourse.component.spec.ts
+++ client/src/app/carApp/choixCourse-component/choixCourse.component.spec.ts
@@ -1,3 +1,36 @@
-describe("Choix Course Component", () => {
+import { TestBed, ComponentFixture } from "../../../../node_modules/@angular/core/testing";
+import { ChoixCourseComponent } from "./choixCourse.component";
+import { GestionnaireBDCourse } from "../baseDeDonnee/GestionnaireBDCourse";
+import { MatExpansionModule } from "../../../../node_modules/@angular/material/expansion";
+import { MatDividerModule } from "../../../../node_modules/@angular/material/divider";
+import { RouterTestingModule } from "../../../../node_modules/@angular/router/testing";
+import { ApercuPisteComponent } from "../apercuPiste/apercuPiste.component";
 
+describe("Choix Course Component", () => {
+    let fixture: ComponentFixture<ChoixCourseComponent>;
+    let component: ChoixCourseComponent;
+    let mockGestionnaireBD: GestionnaireBDCourse;
+
+    beforeEach(() => {
+        mockGestionnaireBD = jasmine.createSpyObj(["obtenirPistes"]);
+        TestBed.configureTestingModule({
+            imports: [MatExpansionModule, MatDividerModule, RouterTestingModule],
+            declarations: [ChoixCourseComponent, ApercuPisteComponent],
+            providers: [
+                { provide: GestionnaireBDCourse, useValue: mockGestionnaireBD },
+            ]
+        });
+
+        fixture = TestBed.createComponent(ChoixCourseComponent);
+        component = fixture.componentInstance;
+    });
+
+    it("Should do nothing", () => {
+        expect(true).toBe(true);
+    });
+
+    afterEach(() => {
+        fixture = null;
+        component = null;
+    });
 });
