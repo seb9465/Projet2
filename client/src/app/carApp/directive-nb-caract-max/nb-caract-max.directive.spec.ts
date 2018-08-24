070b974fbff2be43033dd1027a3e5c8e589b2c7d
SebastienCadorette <cado.94@me.com>
2018-08-23 21:06:35 -0400

    New Nb Caract Max Directive test file.

---
+++ client/src/app/carApp/directive-nb-caract-max/nb-caract-max.directive.spec.ts
@@ -0,0 +1,28 @@
+import { NbCaractMaxDirective } from "./nb-caract-max.directive";
+import { Component, DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
+@Component({
+    template: "<input [appNbCaractMax]='5' categorie='minutes'>"
+})
+class TestNbCaractMaxComponent { }
+
+describe("NbCaractMaxDirective", () => {
+    let component: TestNbCaractMaxComponent;
+    let fixture: ComponentFixture<TestNbCaractMaxComponent>;
+    let inputEl: DebugElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestNbCaractMaxComponent, NbCaractMaxDirective]
+        });
+        fixture = TestBed.createComponent(TestNbCaractMaxComponent);
+        component = fixture.componentInstance;
+        inputEl = fixture.debugElement.query(By.css("input"));
+    });
+
+    it("Should do nothing", () => {
+        expect(true).toBe(true);
+    });
+});
