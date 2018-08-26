cdc043f8292290d640e4ebf94f7fc6ae2dff74de
SebastienCadorette <cado.94@me.com>
2018-08-26 14:00:43 -0400

    New test file for the Tableau Meilleurs Temps Component.

---
+++ client/src/app/carApp/tableau-meilleurs-temps/tableau-meilleurs-temps.component.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from "../../../../node_modules/@angular/core/testing";
+import { TableauMeilleursTempsComponent } from "./tableau-meilleurs-temps.component";
+
+describe("Tableau Meilleurs Temps Component", () => {
+    let fixture: ComponentFixture<TableauMeilleursTempsComponent>;
+    let component: TableauMeilleursTempsComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [],
+            declarations: [
+                TableauMeilleursTempsComponent
+            ],
+            providers: [],
+        });
+    });
+});
