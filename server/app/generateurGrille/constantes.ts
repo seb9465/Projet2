128d137f7fb196395ec26a5db9fc95acecd7c32c
SebastienCadorette <cado.94@me.com>
2018-09-01 12:33:56 -0400

    Makes testing easier.

--- server/app/generateurGrille/constantes.ts
+++ server/app/generateurGrille/constantes.ts
@@ -4,8 +4,10 @@ export const TAILLE_MINIMUM: number = 2;
 export const VIDE: string = "_";
 export const NOIR: string = "0";
 export const CASE_OK: string = "/";
-export const POURCENTAGE_NOIR: number = 0.45;
-export const PROB_CASE_N: number = 0.25;
+// export const POURCENTAGE_NOIR: number = 0.45;
+// export const PROB_CASE_N: number = 0.25;
+export const POURCENTAGE_NOIR: number = 0.9;
+export const PROB_CASE_N: number = 0.9;
 export const HEURISTIQUE_LNG_MOT: number = 1.75;
 
 export const REQUETE_COMMUN: string = "http://localhost:3000/servicelexical/commun/contrainte/";
