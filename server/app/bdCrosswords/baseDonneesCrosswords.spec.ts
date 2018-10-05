dee4f5e8d7758edb2de49ec056af15957afc1473
SebastienCadorette <cado.94@me.com>
2018-10-05 17:56:51 -0400

    somes tests for the crosswords' DB.

--- server/app/bdCrosswords/baseDonneesCrosswords.spec.ts
+++ server/app/bdCrosswords/baseDonneesCrosswords.spec.ts
@@ -1,6 +1,7 @@
-import * as assert from "assert"import { BaseDonneesCrosswords } from "./baseDonneesCrosswords";
-É;
-const URL_SERVICE: string = "http://localhost:3000/crosswords/";
+import * as assert from "assert";
+import { BaseDonneesCrosswords } from "./baseDonneesCrosswords";
+
+// const URL_SERVICE: string = "http://localhost:3000/crosswords/";
 
 describe("Base Donnees Crosswords", () => {
     let service: BaseDonneesCrosswords;
@@ -11,6 +12,32 @@ describe("Base Donnees Crosswords", () => {
 
     it("Should do nothing", () => {
         assert(true);
+    });
+
+    describe("Constructor", () => {
+        it("Should initialize mongoose", (done: MochaDone) => {
+            assert.ok(service["mongoose"]);
+            done();
+        });
+
+        it("Should initialize the schema", (done: MochaDone) => {
+            assert.ok(service["schema"]);
+            done();
+        });
+
+        it("Should initialize the model", (done: MochaDone) => {
+            assert.ok(service["model"]);
+            done();
+        });
+    });
+
+    describe("Connection to the DB", () => {
+        it("Should connect to the DB", (done: MochaDone) => {
+            service["seConnecter"]().then(() => {
+                assert(service["estConnecte"]);
+                done();
+            });
+        });
     });
 
     afterEach(() => {
