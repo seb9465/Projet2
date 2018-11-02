67ebf14cf3e700d50ea02d9686ed2811ef860360
Sebastien Cadorette <cado.94@me.com>
2018-11-02 17:42:31 -0400

    Ajout route de base.

--- client/src/app/Constantes/routeBDCrosswords.ts
+++ client/src/app/Constantes/routeBDCrosswords.ts
@@ -1,1 +1,2 @@
-export const URI_SUPPRESSION_TOUTES_PARTIES: string = "http://localhost:3000/crosswords/supprimerToutesLesParties";
+export const URI_BASE: string = "http://localhost:3000/crosswords/";
+export const URI_SUPPRESSION_TOUTES_PARTIES: string = URI_BASE + "supprimerToutesLesParties";
