3731563905618361c6005cf0a0d69ca0cd5687f4
Sebastien Cadorette <cado.94@me.com>
2018-11-02 17:44:27 -0400

    Ajout route pour obtenir les parties.

--- client/src/app/Constantes/routeBDCrosswords.ts
+++ client/src/app/Constantes/routeBDCrosswords.ts
@@ -1,2 +1,3 @@
 export const URI_BASE: string = "http://localhost:3000/crosswords/";
 export const URI_SUPPRESSION_TOUTES_PARTIES: string = URI_BASE + "supprimerToutesLesParties";
+export const URI_OTENIR_PARTIES: string = URI_BASE + "obtenirParties";
