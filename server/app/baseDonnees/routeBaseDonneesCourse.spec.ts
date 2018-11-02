363d7a3da2076de3ef890b4481e869bcb9590194
Sebastien Cadorette <cado.94@me.com>
2018-11-02 16:50:33 -0400

    Suppression de commentaires & ajustement d'indentation.

--- server/app/baseDonnees/routeBaseDonneesCourse.spec.ts
+++ server/app/baseDonnees/routeBaseDonneesCourse.spec.ts
@@ -1,27 +1,21 @@
-/*
-    Tests des routes.
-    Le serveur doit être en marche pour que les tests fonctionnent.
-*/
-
 import * as assert from "assert";
 import * as WebRequest from "web-request";
 import { PisteBD } from "../../../client/src/app/carApp/piste/IPisteBD";
 
 const URL_SERVICE: string = "http://localhost:3000/apipistes/";
-{
-    describe("Tests sur les routes de la base de données.", () => {
-        describe("Accéder aux routes (serveur doit rouler pour que ca fonctionne)", async () => {
-            describe("Obtention de pistes.", () => {
-                it("Doit retourner un tableau non-vide", async () => {
-                    const PISTES: PisteBD[] = await WebRequest.json<PisteBD[]>(URL_SERVICE);
-                    assert.notEqual(PISTES.length, 0);
-                });
 
-                it("Reception de pistes.", async () => {
-                    const DATA: PisteBD[] = await WebRequest.json<PisteBD[]>(URL_SERVICE);
-                    assert.ok(DATA);
-                });
+describe("Tests sur les routes de la base de données.", () => {
+    describe("Accéder aux routes (serveur doit rouler pour que ca fonctionne)", async () => {
+        describe("Obtention de pistes.", () => {
+            it("Doit retourner un tableau non-vide", async () => {
+                const PISTES: PisteBD[] = await WebRequest.json<PisteBD[]>(URL_SERVICE);
+                assert.notEqual(PISTES.length, 0);
+            });
+
+            it("Reception de pistes.", async () => {
+                const DATA: PisteBD[] = await WebRequest.json<PisteBD[]>(URL_SERVICE);
+                assert.ok(DATA);
             });
         });
     });
-}
+});
