87d30b859bdaebe1d8323df80ba1e6428ec92908
Sebastien Cadorette <cado.94@me.com>
2018-11-02 17:26:11 -0400

    Suppression commentaire.

--- client/src/app/crosswords/config-partie/configurationPartie.ts
+++ client/src/app/crosswords/config-partie/configurationPartie.ts
@@ -5,6 +5,6 @@ export class ConfigurationPartie implements IConfigurationPartie {
     public nombreDeJoueurs: number;
     public niveauDeDifficulte: Difficulte;
 
-    public idPartie: number; // id possiblement pour identifier une partie (en ligne)
+    public idPartie: number;
     public requete: string;
 }
